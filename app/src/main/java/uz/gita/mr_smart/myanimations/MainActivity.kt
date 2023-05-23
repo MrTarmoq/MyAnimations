ba13dba175916504cd909f2592bc4ae417407901
omonov <omonovshohjahon77@gmail.com>
2023-05-23 13:46:56 +0500

    newAnimation

--- app/src/main/java/uz/gita/mr_smart/myanimations/MainActivity.kt
+++ app/src/main/java/uz/gita/mr_smart/myanimations/MainActivity.kt
@@ -2,10 +2,25 @@ package uz.gita.mr_smart.myanimations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txt = findViewById<TextView>(R.id.text)
+        txt.setOnClickListener {
+            exampleAnimation(txt)
+        }
     }
+
+    private fun exampleAnimation(view: View){
+
+    }
+
 }
