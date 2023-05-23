90ec7f3b769b7753fa8b14ef26bc408429b5833e
omonov <omonovshohjahon77@gmail.com>
2023-05-23 19:17:06 +0500

    newAnimation

--- app/src/main/java/uz/gita/mr_smart/myanimations/MainActivity.kt
+++ app/src/main/java/uz/gita/mr_smart/myanimations/MainActivity.kt
@@ -8,19 +8,55 @@ import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import uz.gita.mr_smart.myanimations.databinding.ActivityMainBinding
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val txt = findViewById<TextView>(R.id.text)
-        txt.setOnClickListener {
-            exampleAnimation(txt)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            image1.setOnClickListener {
+                rotationAnimation(it)
+            }
+            image2.setOnClickListener {
+                exampleAnimation(it)
+            }
+
+            image3.setOnClickListener {
+                exampleAnimation1(it)
+            }
+
+            image4.setOnClickListener {
+                exampleAnimation2(it)
+            }
         }
+
     }
 
     private fun exampleAnimation(view: View){
+
+        ValueAnimator.ofFloat(0f, 100f).apply {
+            addUpdateListener {
+                view.alpha = (100 - it.animatedValue as Float) / 100
+            }
+            duration = 2000
+            start()
+        }
+
+        ValueAnimator.ofFloat(0f, 100f).apply {
+            addUpdateListener {
+                view.alpha = (it.animatedValue as Float) / 100
+            }
+            duration = 2000
+            start()
+        }
+    }
+
+    private fun rotationAnimation(view: View){
         ValueAnimator.ofFloat(0f, 360f).apply {
             addUpdateListener {
                 view.rotation = it.animatedValue as Float
@@ -28,6 +64,14 @@ class MainActivity : AppCompatActivity() {
             duration = 2000
             start()
         }
+    }
+
+    private fun exampleAnimation1(view: View){
+
+    }
+
+    private fun exampleAnimation2(view: View){
+
     }
 
 }
